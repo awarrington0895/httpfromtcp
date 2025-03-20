2d9eebc52b65e67fa26f1af2f7d7917d113ddd00
Alex Warrington <awarrington0895@gmail.com>
2025-03-19 19:53:26 -0500

    feat: complete file reading exercise

--- main.go
+++ main.go
@@ -4,32 +4,58 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	file, err := os.Open("./messages.txt")
+const inputFilePath = "messages.txt"
 
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-
-	buf := make([]byte, 8)
-
-	for {
-		_, err = file.Read(buf)
-
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				os.Exit(0)
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
 			}
-
-			panic(err)
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
 
-		fmt.Printf("read: %s\n", string(buf))
+		close(lines)
+		f.Close()
+	}()
+
+	return lines
+}
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
 
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+	lineChannel := getLinesChannel(f)
+
+	for line := range lineChannel {
+		fmt.Printf("read: %s\n", line)
+	}
 }
